ba5aa4462ac1d0f7c79f086ce0adf05369719489
Brock Whitten <brock@chloi.io>
2016-10-17 05:29:23 -0700

    adds colors

---
+++ test/basic.js
@@ -0,0 +1,11 @@
+
+
+var shirt = require("../")
+
+console.log(shirt("foo"))
+console.log(shirt("bar"))
+console.log(shirt("baz"))
+console.log(shirt("bart"))
+console.log(shirt("heyo"))
+console.log(shirt("whatever"))
+console.log(shirt("this is cool"))
